45af619e823a5c3ab893b9dd543c30c5d6f02f50
David Anderson <david.anderson1993@gmail.com>
2021-05-07 14:26:13 -0500

    updating the navbar and react router layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Nav from './Components/Nav/Nav';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Home from './Pages/Home';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Logo</h3>
+        <ul className='nav-links'>
+            <li>CONTACT</li>
+            <li>ABOUT</li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
