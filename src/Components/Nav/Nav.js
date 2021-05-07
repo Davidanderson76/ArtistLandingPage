bb73c9f8e2dd3ebc1d4f35030480340348cc1629
David Anderson <david.anderson1993@gmail.com>
2021-05-07 14:46:25 -0500

    layout ready for design

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './Nav.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <nav>
+
+        <Link className='nav-style' to='/'>
         <h3>Logo</h3>
-        <ul className='nav-links'>
-            <li>CONTACT</li>
-            <li>ABOUT</li>
-        </ul>
-    </nav>
+        </Link>
+            <ul className='nav-links'>
+
+                <Link className='nav-style' to='/contact'>
+                <li>CONTACT</li>
+                </Link>
+
+                <Link className='nav-style' to='/about'>
+                <li>ABOUT</li>
+                </Link>
+
+            </ul>
+    </nav> 
   );
 }
 
